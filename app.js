0e3b5d89356a79e7336c51663b9d28acbc0b07ae
Eric D. Torres <etorresnotary@gmail.com>
2020-09-04 10:00:48 -0500

    new uer logged to the console

--- app.js
+++ app.js
@@ -20,5 +20,13 @@ async function getRandomUser() {
   const res = await fetch("https://randomuser.me/api");
   const data = await res.json();
 
-  console.log(data);
+//   console.log(data);
+
+const user = data.results[0]
+
+const newUser = {
+    name: `${user.name.first} ${user.name.last}`, 
+    money: Math.floor(Math.random() * 1000000)
+}
+console.log(newUser);
 }
