9931bc7ac958571844be215735e82ca68b9c0880
Eric D. Torres <etorresnotary@gmail.com>
2020-09-04 09:57:24 -0500

    fetch used to get random user

---
+++ app.js
@@ -0,0 +1,24 @@
+//Random User API
+//
+
+//select html elements for JS manipulation
+const mainEl = document.querySelector("#main");
+const addUserBtn = document.querySelector("#add-user");
+const doubleBtn = document.querySelector("#double");
+const showMillionairesBtn = document.querySelector("#show-millionaires");
+const sortBtn = document.querySelector("#sort");
+const calculateWealthBtn = document.querySelector("#calculate-wealth");
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+//fetch random user and add money
+async function getRandomUser() {
+  const res = await fetch("https://randomuser.me/api");
+  const data = await res.json();
+
+  console.log(data);
+}
